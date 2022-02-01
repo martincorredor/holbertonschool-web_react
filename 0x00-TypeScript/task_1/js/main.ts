700c0c49fcd3b6c1743436e38d03aacfba9a8677
Martin Corredor <dev.martincorredor@gmail.com>
2022-01-31 21:50:00 -0500

    |ADDED| Build a techer interface

---
+++ 0x00-TypeScript/task_1/js/main.ts
@@ -0,0 +1,9 @@
+//=========== Build a teacher interface ===============
+export interface Teacher {
+	readonly firstName: string;
+	readonly lastName: string;
+	fullTimeEmployee: boolean;
+	yearsOfExperience?: number;
+	location: string;
+	[propName: string]: any;
+}
