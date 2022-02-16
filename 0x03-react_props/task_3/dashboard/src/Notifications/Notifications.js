f839e9a1eac8311ae0b13c56456cfa1f1d6e569f
Martin Corredor <dev.martincorredor@gmail.com>
2022-02-15 21:20:46 -0500

    |ADDED| Task 3. Checking the application using the React extension

---
+++ 0x03-react_props/task_3/dashboard/src/Notifications/Notifications.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import './Notifications.css'
+import NotificationItem from './NotificationItem'
+import icon from '../assets/close-icon.png'
+import { getLatestNotification } from '../utils/utils'
+
+const Notifications = () => {
+	function clickClose() {
+		console.log('Close button has been clicked')
+	}
+	const htmlObj = {
+		__html: getLatestNotification()
+	}
+	return (
+		<div className='Notifications'>
+			<p style={{ display: 'inline' }}>Here is the list of notifications</p>
+			<button style={{ float: 'right' }} aria-label="Close" onClick={clickClose}>
+				<img src={icon} alt="" style={{ height: '3vh'}} />
+			</button>
+			<ul>
+				<NotificationItem type="urgent" value="New course available"/>
+				<NotificationItem type="urgent" value="New resume available"/>
+				<NotificationItem type="urgent" html={htmlObj}/>
+			</ul>
+		</div>
+	)
+}
+
+export default Notifications
